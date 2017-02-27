53dca954d6db980dfb124c82c573d0d87fcefd75
NightFeather <ss900328@gmail.com>
2017-02-27 12:39:45 +0800

    Change VueRouter to html5 history mode
    Currently not using though

--- src/main.js
+++ src/main.js
@@ -3,10 +3,11 @@ import VueRouter from 'vue-router'
 import App from './App'
 import 'font-awesome-webpack'
 
-Vue.use(VueRouter)
+var router = new VueRouter({ mode: 'history' })
 
 // eslint-disable-next-line no-new
 new Vue({
+  router,
   el: '#app',
   template: '<app/>',
   data: function () {
