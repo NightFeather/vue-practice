7f8d599b44c5e2f48924b7c6e1074a378565aba9
NightFeather <ss900328@gmail.com>
2017-02-27 09:46:59 +0800

    Replace the image with FontAwesome Icon

--- src/components/Card/Thumbnail.vue
+++ src/components/Card/Thumbnail.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="thumbnail">
-    <img v-bind:src="imageSrc(card)" alt="">
+  <div class="thumbnail" v-bind:class="thumbnailType">
+    <img v-show="thumbnailType == 'image'" v-bind:src="imageSrc(card)" alt="">
   </div>
 </template>
 
@@ -21,6 +21,18 @@
       }
     },
     computed: {
+      thumbnailType: function () {
+        var url = this.card.url
+        if (url === undefined || url === '') {
+//        '/static/images/discussion.png'
+          return 'discussion'
+        } else if (['jpg', 'jpeg', 'png', 'gif'].some(function (v) { return url.endsWith(v) })) {
+          return 'image'
+        } else {
+//        '/static/images/document.png'
+          return 'document'
+        }
+      }
     }
   }
 </script>
@@ -28,8 +40,22 @@
 <style lang="scss">
   
   .thumbnail{
-
+    font-size: 65px;
     min-width: 75px;
+    text-align: center;
+
+    &.document::after {
+      font-family: FontAwesome;
+      content: '\f075';
+      min-width: 75px;
+      color: black;
+    }
+    &.discussion::after {
+      font-family: FontAwesome;
+      content: '\f15b';
+      min-width: 75px;
+      color: black;
+    }
 
     img {
       height: 75px;
